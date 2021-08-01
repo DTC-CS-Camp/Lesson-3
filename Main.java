fe0df51b31bf3380d8f38f7d1f239a0defe6b134
DTC-CS-Camp <68837059+DTC-CS-Camp@users.noreply.github.com>
2021-08-01 15:25:45 -0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,1 +1,26 @@
+import java.io.*;
+import java.util.*;
+class Main {
+  public static void main(String[] args) {
+  //EDIT CODE BELOW \/
 
+  //problem 1 below
+  System.out.println("Problem 1: Enter a positive integer:");
+  
+ 
+  //problem 2 below
+    
+    
+    
+    
+    
+  //problem 3 below
+  
+
+
+
+
+
+  //EDIT CODE BELOW \/
+  }
+}
